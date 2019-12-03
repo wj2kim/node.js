088ef612c06aeab2b24d4a4df4960f7f32e8ec08
Paul Woo Jung Kim <paulwoojungkim@Pauls-MacBook-Pro.local>
2019-12-03 20:19:57 +0900

    first commit

---
+++ template.js
@@ -0,0 +1,5 @@
+var name='김우정';
+
+var para=`A paragraph is a self-contained unit of ${name} a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more ${name} sentences. Though not required by the syntax of any language, paragraphs are usually.${name}`+name;
+
+console.log(para);
