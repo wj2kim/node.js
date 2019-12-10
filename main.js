ef86c26224b765e11cf3701b0748c40cb1ce9330
paulwoojungkim <wj2kim@gmail.com>
2019-12-10 19:52:06 +0900

    node practice commit

--- main.js
+++ main.js
@@ -1,37 +1,70 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var inte=2;
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title=queryData.id;
+    var pathname=url.parse(_url,true).pathname;
     
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
-      var template = `
-      <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=Javascript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-  `
-  response.writeHead(200);
-  response.end(template);
-    });
- //커밋 되나 테스트
- //이젠 되야해 테스트
+    if(pathname === '/'){
+    if(queryData.id === undefined){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=Javascript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `
+        response.writeHead(200);
+        response.end(template);
+      });
+    }else{
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+        var title=queryData.id;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=Javascript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `
+        response.writeHead(200);
+        response.end(template);
+      });
+    }      
+    }else{
+      response.writeHead(404);
+      response.end('Not found');
+    }
+
+
 });
 app.listen(3000);
