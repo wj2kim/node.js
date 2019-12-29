8a9dbf7cf86ba8b5306523429f618422d6193530
paulwoojungkim <wj2kim@gmail.com>
2019-12-29 15:12:41 +0900

    practice commit

---
+++ module/readFile.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('module/readme.txt',(err,data)=>{
+    if(err){
+        throw err;
+    }
+    console.log(data);
+    console.log(data.toString());
+});
+
+// 즉 readFile의 형식은 버퍼의 형식으로 제공된다는 것을 알 수 있다.
