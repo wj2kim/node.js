ca7f1a922d8edf5e6885a88b317f137a521dad5d
paulwoojungkim <wj2kim@gmail.com>
2020-01-04 17:47:00 +0900

    update commit

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?=${title}">update</a>`);
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
             response.writeHead(200);
             response.end(template);
           });
@@ -65,7 +65,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -89,12 +89,58 @@ var app = http.createServer(function(request,response){
           var description = post.description
           fs.writeFile(`data/${title}`,description,'utf-8',(err)=>{
             if(err) throw err;
-            console.log('The file has been saved');
+            // response.writeHaed(302,{Location:'/'}) 페이지 리다이렉트 코드
             response.writeHead(302,{Location:`/?id=${title}`});
             response.end();
           });
       });
-    } else {
+    }else if(pathname === '/update'){
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+            <input type='hidden' name='id' value='${title}'>
+            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?=${title}">update</a>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    } else if(pathname='/update_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          console.log(id);
+          var title = post.title;
+          console.log(title);
+          var description = post.description
+          console.log(description);
+          fs.rename(`data/${id}`,`data/${title}`,(err)=>{
+            if(err) throw err;
+            fs.writeFile(`data/${title}`,description,'utf-8',(err)=>{
+              if(err) throw err;
+              response.writeHead(302,{Location:`/?id=${title}`});
+              response.end();
+            });
+            // response.writeHaed(302,{Location:'/'}) 페이지 리다이렉트 코드
+          });
+      });
+    }else {
       response.writeHead(404);
       response.end('Not found');
     }
